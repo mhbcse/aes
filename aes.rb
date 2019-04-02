63205c807e5d2914f9387aba6206dcd9139272c3
Maruf Hasan <mhb.cse@gmail.com>
2019-04-03 00:41:26 +0600

    Aes encryption

--- aes.rb
+++ aes.rb
@@ -1,26 +1,5 @@
 class Aes
-  RCON = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36,
-          0x6c, 0xd8, 0xab, 0x4d, 0x9a, 0x2f, 0x5e, 0xbc, 0x63, 0xc6,
-          0x97, 0x35, 0x6a, 0xd4, 0xb3, 0x7d, 0xfa, 0xef, 0xc5, 0x91]
-
-  S_BOX = [
-    [0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76],
-    [0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0],
-    [0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15],
-    [0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75],
-    [0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84],
-    [0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf],
-    [0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8],
-    [0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2],
-    [0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73],
-    [0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb],
-    [0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79],
-    [0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08],
-    [0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a],
-    [0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e],
-    [0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf],
-    [0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]
-  ]
+  RCON = ['01', '02', '04', '08', '10', '20', '40', '80', '1b', '36']
 
   S_BOX_HEX = [["63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76"],
                ["ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0"],
@@ -38,25 +17,6 @@ class Aes
                ["70", "3e", "b5", "66", "48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e"],
                ["e1", "f8", "98", "11", "69", "d9", "8e", "94", "9b", "1e", "87", "e9", "ce", "55", "28", "df"],
                ["8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"]]
-
-  REVERSE_S_BOX = [
-    [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb],
-    [0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb],
-    [0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e],
-    [0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25],
-    [0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92],
-    [0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84],
-    [0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06],
-    [0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b],
-    [0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73],
-    [0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e],
-    [0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b],
-    [0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4],
-    [0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f],
-    [0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef],
-    [0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61],
-    [0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d]
-  ]
 
   REVERSE_S_BOX_HEX = [["52", "09", "6a", "d5", "30", "36", "a5", "38", "bf", "40", "a3", "9e", "81", "f3", "d7", "fb"],
                        ["7c", "e3", "39", "82", "9b", "2f", "ff", "87", "34", "8e", "43", "44", "c4", "de", "e9", "cb"],
@@ -90,22 +50,229 @@ class Aes
   end
 
   def encrypt(plain_text)
-  # ToDo: break into block and add pad to make 128 bit block
+    puts plain_text
+    blocks = break_into_blocks(plain_text)
+    puts blocks.inspect
+    # convert_block_into_words(blocks.first)
+    blocks = blocks.collect { |block| convert_block_into_words(block) }
+    puts blocks.inspect
+
+    output_cipher_blocks = []
+    blocks.each_with_index do |block, index|
+      puts "Block #{index + 1}------------------"
+      puts "Input Block: #{block}"
+
+      # First add round key before rounds
+      key_words = @key_words[0..3]
+      add_round_key_output = add_round_key(block, key_words)
+
+      cipher_block = encrypt_rounds(add_round_key_output, 1)
+      puts "Cipher Block: #{cipher_block.inspect}"
+
+      output_cipher_blocks << cipher_block
+    end
+
+    puts 'Output cipher blocks -------------'
+    puts output_cipher_blocks.inspect
+
+    # ToDo: Create hex stream from hex blocks
+    cipher_text = combine_hex_blocks(output_cipher_blocks)
+
+    return cipher_text
   end
 
-  def decrypt(cipher)
+  def encrypt_rounds(input_block, round)
+    # Substitute Bytes step
+    round_substitute_output = input_block.collect { |word| substitue_bytes(word) }
+    # puts round_substitute_output.inspect
 
+    # Shift rows step
+    round_shift_rows_output = shift_rows(round_substitute_output)
+    # puts round_shift_rows_output.inspect
+
+    if round < 10
+      # Mix Columns step
+      round_mix_column_ouput = mix_columns(round_shift_rows_output)
+    else
+      round_mix_column_ouput = round_shift_rows_output
+    end
+
+    # Add round key step
+    key_words = @key_words[(round*4)..(round*4+3)]
+    round_add_round_key_output = add_round_key(round_mix_column_ouput, key_words)
+    # puts round_add_round_key_output.inspect
+
+    if round == 10
+      return round_add_round_key_output
+    else
+      return encrypt_rounds(round_add_round_key_output, round + 1)
+    end
+  end
+
+  def break_into_blocks(text)
+    blocks = text.scan(/.{1,16}/)
+    last_block = blocks.last
+    if last_block.length < 16
+      last_block = last_block.ljust(16, '{')
+      blocks.pop
+      blocks << last_block
+    end
+  end
+
+  def convert_block_into_words(block)
+    hex_array = string_to_hex_array(block)
+    # puts hex_array.inspect
+    hex_block = hex_array.each_slice(4).to_a
+    # puts hex_block.inspect
+    return hex_block
+  end
+
+  # Add round key XOR operation
+  def add_round_key(block, key_words)
+    # bitwise XOR between input_text and key
+    output_block = []
+    block.zip(key_words).each do |hex_word, hex_key_word|
+      xor_data = hex_word_xor(hex_word, hex_key_word)
+      output_block << xor_data
+    end
+    return output_block
+  end
+
+  # Replace bytes using S Box
+  def substitue_bytes(hex_word)
+    new_hex_word = []
+
+    hex_word.each do |hex_byte|
+      x, y = hex_byte.split('').map { |e| hex_to_int(e) }
+      new_hex_word << S_BOX_HEX[x][y]
+    end
+
+    return new_hex_word
+  end
+
+  def shift_rows(block)
+    output_block = []
+    block.each_with_index do |word, index|
+      if index > 0
+        output_word = word.rotate(index)
+      else
+        output_word = word
+      end
+      output_block << output_word
+    end
+
+    return output_block
+  end
+
+  def mix_columns(block)
+
+
+    return block
+  end
+
+  def combine_hex_blocks(blocks)
+    hex_array = blocks.flatten
+    # puts hex_array.inspect
+    hex_array = hex_array.map{|hex_byte| "0x#{hex_byte}" }
+    # puts hex_array.inspect
+    return hex_array.join('')
+  end
+
+  def decrypt(cipher_text)
+    puts cipher_text
+    blocks = break_cipher_texts_to_block(cipher_text)
+    puts blocks.inspect
+
+    output_plain_text_blocks = []
+
+    blocks.each_with_index do |block, index|
+      puts "Block #{index + 1}------------------"
+      puts "Input Block: #{block}"
+
+      # First add round key before rounds
+      key_words = @key_words[40..43]
+      add_round_key_output = add_round_key(block, key_words)
+
+      plain_text_block = decrypt_rounds(add_round_key_output, 1)
+      puts "Plain Text Block: #{plain_text_block.inspect}"
+
+      output_plain_text_blocks << plain_text_block
+    end
+
+    puts 'Output cipher blocks -------------'
+    puts output_plain_text_blocks.inspect
+
+    # Combine Plain Text blocks
+    plain_text = combine_plain_text_blocks(output_plain_text_blocks)
+
+    return plain_text
+  end
+
+  def break_cipher_texts_to_block(cipher_text)
+    hex_array = cipher_text.split('0x').drop(1)
+    blocks = hex_array.each_slice(16).to_a
+    blocks = blocks.collect{|block| block.each_slice(4).to_a }
+    # puts blocks.inspect
+    return blocks
+  end
+
+  def decrypt_rounds(input_block, round)
+    output_block = []
+
+    # Shift rows step
+    round_shift_rows_output = inverse_shift_rows(input_block)
+    # puts round_shift_rows_output.inspect
+
+    # Substitute Bytes step
+    round_substitute_output = round_shift_rows_output.collect { |word| substitue_bytes(word) }
+    # puts round_substitute_output.inspect
+
+    # Add round key step
+    key_words = @key_words[(round*4)..(round*4+3)]
+    round_add_round_key_output = add_round_key(round_substitute_output, key_words)
+    # puts round_add_round_key_output.inspect
+
+    if round < 10
+      # Mix Columns step
+      round_mix_column_ouput = mix_columns(round_add_round_key_output)
+    else
+      round_mix_column_ouput = round_add_round_key_output
+    end
+
+    if round == 10
+      return round_mix_column_ouput
+    else
+      return decrypt_rounds(round_mix_column_ouput, round + 1)
+    end
+  end
+
+  def combine_plain_text_blocks(blocks)
+    hex_array = blocks.flatten
+    # puts hex_array.inspect
+    return hex_array_to_string(hex_array)
+  end
+
+  def inverse_shift_rows(block)
+    output_block = []
+    block.each_with_index do |word, index|
+      if index > 0
+        output_word = word.rotate(index)
+      else
+        output_word = word
+      end
+      output_block << output_word
+    end
+
+    return output_block
   end
 
   def key_expansion
-    puts @key
+    puts "Original Key: #{@key}"
     hex_array = string_to_hex_array(@key)
-    puts hex_array.inspect
+    puts "Key converted to Hex: #{hex_array.inspect}"
 
     @key_words = []
     hex_array.each_slice(4) { |a| @key_words << a }
-
-    puts @key_words.inspect
 
     (4..43).each do |i|
       temp = @key_words[i-1]
@@ -113,10 +280,11 @@ class Aes
       @key_words[i] = hex_word_xor(@key_words[i-4], temp)
     end
 
-    puts @key_words.inspect
+    puts "Expanded Key: #{@key_words.inspect}"
   end
 
   def sub_hex_word(hex_word)
+    # Substitue bytes using S Box
     return substitue_bytes(hex_word)
   end
 
@@ -126,7 +294,9 @@ class Aes
   end
 
   def rcon_hex_word(index)
-    return ['1B', '00', '00', '00']
+    rc = RCON[index-1]
+    rcon = [rc, '00', '00', '00']
+    return rcon
   end
 
   def hex_word_xor(hex_word1, hex_word2)
@@ -144,24 +314,6 @@ class Aes
     int_xor = int_byte1 ^ int_byte2
     hex_xor = int_to_hex(int_xor)
     return hex_xor
-  end
-
-  def add_round_key(input_text, key)
-    # bitwise XOR between input_text and key
-
-  end
-
-  # Replace bytes using S Box
-  def substitue_bytes(hex_word)
-    new_hex_word = []
-
-    hex_word.each do |hex_byte|
-      y,x = hex_byte.split('').map{|e| hex_to_int(e)}
-      # ToDo: Need check s box value
-      new_hex_word << S_BOX_HEX[x][y]
-    end
-
-    return new_hex_word
   end
 
   def string_to_hex_array(text)
@@ -192,6 +344,10 @@ class Aes
   #   unicodes.map(&:chr).join('')
   # end
 
+  def hex_array_to_string(hex_array)
+    hex_array.map { |e| e.to_i 16 }.map(&:chr).join('')
+  end
+
   def hex_to_binary(hex)
     '%08b' % hex.to_i(16)
   end
@@ -201,10 +357,15 @@ class Aes
   end
 end
 
-key = Aes.generate_random_key
-puts key
+# key = Aes.generate_random_key
+# puts key
+key = 'HEohOoOLrVwaECVv'
 aes = Aes.new(key)
+cipher_text = aes.encrypt('Hello, This is maruf here. I want to introduce myself with you!njjbuiyiy  !@##@#$##$$$')
+puts "Cipher Text: #{cipher_text}"
 
+plain_text = aes.decrypt(cipher_text)
+puts "Plain Text: #{plain_text}"
 
 # bytes = aes.string_to_bytes('Hello,Bangladesh')
 # puts "Bytes: #{bytes.inspect}"
